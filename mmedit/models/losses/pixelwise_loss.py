b0946add3aaa69bb5637a64edf3f1f3fc25d0159
ZhangQX <1216155858@qq.com>
2025-05-20 11:47:02 +0800

    Add files via upload

---
+++ mmedit/models/losses/pixelwise_loss.py
@@ -0,0 +1,346 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from ..registry import LOSSES
+from .utils import masked_loss
+
+_reduction_modes = ['none', 'mean', 'sum']
+
+
+@masked_loss
+def l1_loss(pred, target):
+    """L1 loss.
+
+    Args:
+        pred (Tensor): Prediction Tensor with shape (n, c, h, w).
+        target ([type]): Target Tensor with shape (n, c, h, w).
+
+    Returns:
+        Tensor: Calculated L1 loss.
+    """
+    return F.l1_loss(pred, target, reduction='none')
+
+
+@masked_loss
+def mse_loss(pred, target):
+    """MSE loss.
+
+    Args:
+        pred (Tensor): Prediction Tensor with shape (n, c, h, w).
+        target ([type]): Target Tensor with shape (n, c, h, w).
+
+    Returns:
+        Tensor: Calculated MSE loss.
+    """
+    return F.mse_loss(pred, target, reduction='none')
+
+
+@masked_loss
+def charbonnier_loss(pred, target, eps=1e-12):
+    """Charbonnier loss.
+
+    Args:
+        pred (Tensor): Prediction Tensor with shape (n, c, h, w).
+        target ([type]): Target Tensor with shape (n, c, h, w).
+
+    Returns:
+        Tensor: Calculated Charbonnier loss.
+    """
+    return torch.sqrt((pred - target)**2 + eps)
+
+
+@LOSSES.register_module()
+class L1Loss(nn.Module):
+    """L1 (mean absolute error, MAE) loss.
+
+    Args:
+        loss_weight (float): Loss weight for L1 loss. Default: 1.0.
+        reduction (str): Specifies the reduction to apply to the output.
+            Supported choices are 'none' | 'mean' | 'sum'. Default: 'mean'.
+        sample_wise (bool): Whether calculate the loss sample-wise. This
+            argument only takes effect when `reduction` is 'mean' and `weight`
+            (argument of `forward()`) is not None. It will first reduce loss
+            with 'mean' per-sample, and then it means over all the samples.
+            Default: False.
+    """
+
+    def __init__(self, loss_weight=1.0, reduction='mean', sample_wise=False):
+        super().__init__()
+        if reduction not in ['none', 'mean', 'sum']:
+            raise ValueError(f'Unsupported reduction mode: {reduction}. '
+                             f'Supported ones are: {_reduction_modes}')
+
+        self.loss_weight = loss_weight
+        self.reduction = reduction
+        self.sample_wise = sample_wise
+
+    def forward(self, pred, target, weight=None, **kwargs):
+        """Forward Function.
+
+        Args:
+            pred (Tensor): of shape (N, C, H, W). Predicted tensor.
+            target (Tensor): of shape (N, C, H, W). Ground truth tensor.
+            weight (Tensor, optional): of shape (N, C, H, W). Element-wise
+                weights. Default: None.
+        """
+        return self.loss_weight * l1_loss(
+            pred,
+            target,
+            weight,
+            reduction=self.reduction,
+            sample_wise=self.sample_wise)
+
+
+@LOSSES.register_module()
+class MSELoss(nn.Module):
+    """MSE (L2) loss.
+
+    Args:
+        loss_weight (float): Loss weight for MSE loss. Default: 1.0.
+        reduction (str): Specifies the reduction to apply to the output.
+            Supported choices are 'none' | 'mean' | 'sum'. Default: 'mean'.
+        sample_wise (bool): Whether calculate the loss sample-wise. This
+            argument only takes effect when `reduction` is 'mean' and `weight`
+            (argument of `forward()`) is not None. It will first reduces loss
+            with 'mean' per-sample, and then it means over all the samples.
+            Default: False.
+    """
+
+    def __init__(self, loss_weight=1.0, reduction='mean', sample_wise=False):
+        super().__init__()
+        if reduction not in ['none', 'mean', 'sum']:
+            raise ValueError(f'Unsupported reduction mode: {reduction}. '
+                             f'Supported ones are: {_reduction_modes}')
+
+        self.loss_weight = loss_weight
+        self.reduction = reduction
+        self.sample_wise = sample_wise
+
+    def forward(self, pred, target, weight=None, **kwargs):
+        """Forward Function.
+
+        Args:
+            pred (Tensor): of shape (N, C, H, W). Predicted tensor.
+            target (Tensor): of shape (N, C, H, W). Ground truth tensor.
+            weight (Tensor, optional): of shape (N, C, H, W). Element-wise
+                weights. Default: None.
+        """
+        return self.loss_weight * mse_loss(
+            pred,
+            target,
+            weight,
+            reduction=self.reduction,
+            sample_wise=self.sample_wise)
+
+@LOSSES.register_module()
+class PEALoss(nn.Module):
+    def __init__(self,loss_weight=1.0):
+        super(PEALoss, self).__init__()
+
+        self.loss_weight = loss_weight
+        self.psnr = PSNRLoss()
+
+    def forward(self, x, y):
+        # Compute attention map
+        attention_map = torch.sigmoid(torch.mean(x, dim=1, keepdim=True))
+        weighted_x = x * attention_map
+        weighted_y = y * attention_map
+        loss = self.loss_weight * self.psnr(weighted_x, weighted_y)
+        return loss
+
+# Example usage:
+# local_attention_loss = LocalAttentionLoss()
+# loss = local_attention_loss(predicted_image, ground_truth_image)
+
+
+@LOSSES.register_module()
+class CharbonnierLoss(nn.Module):
+    """Charbonnier loss (one variant of Robust L1Loss, a differentiable
+    variant of L1Loss).
+
+    Described in "Deep Laplacian Pyramid Networks for Fast and Accurate
+        Super-Resolution".
+
+    Args:
+        loss_weight (float): Loss weight for L1 loss. Default: 1.0.
+        reduction (str): Specifies the reduction to apply to the output.
+            Supported choices are 'none' | 'mean' | 'sum'. Default: 'mean'.
+        sample_wise (bool): Whether calculate the loss sample-wise. This
+            argument only takes effect when `reduction` is 'mean' and `weight`
+            (argument of `forward()`) is not None. It will first reduces loss
+            with 'mean' per-sample, and then it means over all the samples.
+            Default: False.
+        eps (float): A value used to control the curvature near zero.
+            Default: 1e-12.
+    """
+
+    def __init__(self,
+                 loss_weight=1.0,
+                 reduction='mean',
+                 sample_wise=False,
+                 eps=1e-12):
+        super().__init__()
+        if reduction not in ['none', 'mean', 'sum']:
+            raise ValueError(f'Unsupported reduction mode: {reduction}. '
+                             f'Supported ones are: {_reduction_modes}')
+
+        self.loss_weight = loss_weight
+        self.reduction = reduction
+        self.sample_wise = sample_wise
+        self.eps = eps
+
+    def forward(self, pred, target, weight=None, **kwargs):
+        """Forward Function.
+
+        Args:
+            pred (Tensor): of shape (N, C, H, W). Predicted tensor.
+            target (Tensor): of shape (N, C, H, W). Ground truth tensor.
+            weight (Tensor, optional): of shape (N, C, H, W). Element-wise
+                weights. Default: None.
+        """
+        return self.loss_weight * charbonnier_loss(
+            pred,
+            target,
+            weight,
+            eps=self.eps,
+            reduction=self.reduction,
+            sample_wise=self.sample_wise)
+
+
+@LOSSES.register_module()
+class MaskedTVLoss(L1Loss):
+    """Masked TV loss.
+
+        Args:
+            loss_weight (float, optional): Loss weight. Defaults to 1.0.
+    """
+
+    def __init__(self, loss_weight=1.0):
+        super().__init__(loss_weight=loss_weight)
+
+    def forward(self, pred, mask=None):
+        """Forward function.
+
+        Args:
+            pred (torch.Tensor): Tensor with shape of (n, c, h, w).
+            mask (torch.Tensor, optional): Tensor with shape of (n, 1, h, w).
+                Defaults to None.
+
+        Returns:
+            [type]: [description]
+        """
+        y_diff = super().forward(
+            pred[:, :, :-1, :], pred[:, :, 1:, :], weight=mask[:, :, :-1, :])
+        x_diff = super().forward(
+            pred[:, :, :, :-1], pred[:, :, :, 1:], weight=mask[:, :, :, :-1])
+
+        loss = x_diff + y_diff
+
+        return loss
+
+
+def torchPSNR(tar_img, prd_img):
+    imdff = torch.clamp(prd_img,0,1) - torch.clamp(tar_img,0,1)
+    rmse = (imdff**2).mean().sqrt()
+    ps = 20*torch.log10(1/rmse)
+    return ps
+
+@LOSSES.register_module()
+class PSNRLoss(nn.Module):
+    def __init__(self, loss_weight=1.0, reduction='mean', toY=False):
+        super(PSNRLoss, self).__init__()
+        assert reduction == 'mean'
+        self.loss_weight = loss_weight
+        self.scale = 10 / np.log(10)
+        self.toY = toY
+        self.coef = torch.tensor([65.481, 128.553, 24.966]).reshape(1, 3, 1, 1)
+        self.first = True
+
+    def forward(self, pred, target):
+
+        if self.toY:
+            if self.first:
+                self.coef = self.coef.to(pred.device)
+                self.first = False
+
+            pred = (pred * self.coef).sum(dim=1).unsqueeze(dim=1) + 16.
+            target = (target * self.coef).sum(dim=1).unsqueeze(dim=1) + 16.
+
+            pred, target = pred / 255., target / 255.
+            pass
+
+
+        return self.loss_weight * self.scale * torch.log(((pred - target) ** 2).mean(dim=(1, 2, 3)) + 1e-8).mean()
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+@LOSSES.register_module()
+class SSIM(nn.Module):
+    def __init__(self, loss_weight=1.0, reduction='mean', toY=False):
+        super(SSIM, self).__init__()
+        assert reduction == 'mean'
+        self.loss_weight = loss_weight
+        self.toY = toY
+        self.first = True
+
+    def forward(self, pred, target):
+        if self.toY:
+            if self.first:
+                # Y通道的系数
+                self.coef = torch.tensor([65.481, 128.553, 24.966]).reshape(1, 3, 1, 1).to(pred.device)
+                self.first = False
+
+            # 将预测值和目标值转换为Y通道（亮度）表示
+            pred = (pred * self.coef).sum(dim=1).unsqueeze(dim=1) + 16.
+            target = (target * self.coef).sum(dim=1).unsqueeze(dim=1) + 16.
+
+            pred, target = pred / 255., target / 255.
+
+        # 计算 SSIM
+        ssim_loss = 1 - self._ssim(pred, target)
+
+        return self.loss_weight * ssim_loss
+
+    def _ssim(self, img1, img2, window_size=11, sigma=1.5):
+        window = self._create_window(window_size, sigma, img1.device)
+
+        # 调整输入张量的维度以匹配 F.conv2d 的要求
+        img1 = img1.squeeze(0) if img1.dim() == 5 else img1
+        img2 = img2.squeeze(0) if img2.dim() == 5 else img2
+
+        mu1 = F.conv2d(img1, window, padding=window_size // 2)
+        mu2 = F.conv2d(img2, window, padding=window_size // 2)
+
+        mu1_sq = mu1.pow(2)
+        mu2_sq = mu2.pow(2)
+        mu1_mu2 = mu1 * mu2
+
+        sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size // 2) - mu1_sq
+        sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size // 2) - mu2_sq
+        sigma12 = F.conv2d(img1 * img2, window, padding=window_size // 2) - mu1_mu2
+
+        C1 = 0.01 ** 2
+        C2 = 0.03 ** 2
+
+        ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+
+        return ssim_map.mean()
+
+    def _create_window(self, window_size, sigma, device):
+        channels = 1
+        gauss = torch.exp(-(torch.arange(window_size, device=device) - window_size // 2) ** 2 / (2 * sigma ** 2))
+        gauss = gauss / gauss.sum()
+        gauss_2d = gauss.unsqueeze(0)  # 保留一个维度，使其成为二维张量
+        window = gauss_2d.mm(gauss_2d.t()).float().unsqueeze(0)
+        return window.expand(channels, 1, window_size, window_size)
+
+
+
+
+
+
+
